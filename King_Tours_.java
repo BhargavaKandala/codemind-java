02f6c8d6a9883f5e37a32bc29b308266d25c8885
Bhargav_Sharma <144446705+BhargavaKandala@users.noreply.github.com>
2024-08-05 05:55:44 +0530

    import java.util.*;
    public class Hello{
        public static void main(String[] args){
            int a,b;
            Scanner read = new Scanner(System.in);
            a = read.nextInt();
            b = read.nextInt();
            System.out.printf("%d", 5*a + 7*b);
        }
    }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Hello{
+    public static void main(String[] args){
+        int a,b;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        System.out.printf("%d", 5*a + 7*b);
+    }
+}
